2e698c086eb10d85973f01cae1a5600556b0c3d6
Shubanu-it21 <95203260+Shubanu-it21@users.noreply.github.com>
2023-08-23 18:06:18 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Find places</title>
+    <link rel= "stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css ">
+    <link rel="stylesheet" href="VIKAS1.css">
+</head>
+<body>
+    <section id="header">
+    <a href="#"><img src="img/logo.png" class="logo" alt=""></a>
+    <div>
+        <u1 id="navbar">
+            <li><a class="active" href="VIKAS.html">Home</a></li>
+            <li><a href="index1.html">Map</av></li>
+            <li><a href="register_form.php">Register</a></li>
+            <li><a href="map.html">Explore Here</a></li>
+            
+            <li><a href="login_form.php">Login</a></li>
+            
+            <li id="lg-bag"> <a href="logout.php"><i class="fa-solid fa-bag-shopping"></i></a></li>
+            <a href="logout.php" id="close"><i class="fa-regular fa-circle-xmark"></i></a>
+        </u1>
+    </div>
+    <div id="mobile">
+        
+        <a href="cart.html"><i class="fa-solid fa-bag-shopping"></i></a>
+        <i id="bar" class="fas fa-outdent"></i>
+    </div>
+    </section>
+    <section id="hero">
+        
+        <h2>Explore the World with us</h2>
+        
+        <p>Reach your destination...</p>
+       <a href="map.html">Explore here</a>
+
+    </section>
+   
+
+ <script src="vikasop.js"></script>   
+</body>
+</html>
